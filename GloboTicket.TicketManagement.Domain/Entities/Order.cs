f46daae0d5e84e7499facc87fb0b965e276a8d95
ruudzaki <kostybelous@gmail.com>
2024-03-24 20:56:27 +0200

    add domain project

---
+++ GloboTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿using GloboTicket.TicketManagement.Domain.Common;
+
+namespace GloboTicket.TicketManagement.Domain.Entities;
+
+public class Order : AuditableEntity
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public int OrderTotal { get; set; }
+    public DateTime OrderPlaced { get; set; }
+    public bool OrderPaid { get; set; }
+}
