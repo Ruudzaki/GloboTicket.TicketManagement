46814ea7d3cdbad55f82273a63794aa0cc436aae
ruudzaki <kostybelous@gmail.com>
2024-03-28 22:29:29 +0200

    add persistence layer

---
+++ GloboTicket.TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,19 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories;
+
+public class EventRepository : BaseRepository<Event>, IEventRepository
+{
+    public EventRepository(GloboTicketDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+    {
+        var matches = _dbContext.Events.AnyAsync(e => e.Name.Equals(name) &&
+                                                      e.Date.Date.Equals(eventDate.Date));
+        return matches;
+    }
+}
