5a312d884075a62ae3d4fc593f5630feaf5bc08e
ruudzaki <kostybelous@gmail.com>
2024-04-13 10:28:21 +0300

    add email service to infrastructure project

---
+++ GloboTicket.TicketManagement.Application/Models/Mail/Email.cs
@@ -0,0 +1,8 @@
+﻿namespace GloboTicket.TicketManagement.Application.Models.Mail;
+
+public class Email
+{
+    public string To { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
---
+++ GloboTicket.TicketManagement.Application/Models/Mail/EmailSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GloboTicket.TicketManagement.Application.Models.Mail;
+
+public class EmailSettings
+{
+    public string ApiKey { get; set; } = string.Empty;
+    public string FromAddress { get; set; } = string.Empty;
+    public string FromName { get; set; } = string.Empty;
+}
